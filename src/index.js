5a8c8e0ca02182afefa0696f2c85c079679da1b1
senzosenkosidlamini <senzosenkosimoyo@gmail.com>
2024-11-28 16:23:16 +0200

    updated the index.html file, index.js file and style.css file

--- src/index.js
+++ src/index.js
@@ -1,48 +1,58 @@
-function updateWeather(response){
-let temperatureElement = document.querySelector("#temperature");
-let temperature = response.data.temperature.current;
-let cityElement = document.querySelector("#city");
-let descriptionElement = document.querySelector("#description");
-let humidityElement =document.querySelector("#humidity");
-let windSpeedElement = document.querySelector("#wind-speed");
-let timeElement = document.querySelector("#time");
-let date = new Date(response.data.time * 1000);
-let iconElement = document.querySelector("#icon");
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-cityElement.innerHTML = response.data.city;
-timeElement.innerHTML = formatDate(date);
-descriptionElement.innerHTML = response.data.condition.description;
-humidityElement.innerHTML = '${response.data.temperature.humidity}%';
-windSpeedElement.innerHTML = '${response.data.wind.speed}km/h';
-temperatureElement.innerHTML = Math.round(temperature);
-iconElement.innerHTML ='<img src="${response.data.condition.icon_url"class="weather-app-icon"/>';
-
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = "${response.data.temperature.humidity}%";
+  windSpeedElement.innerHTML = "${response.data.wind.speed}km/h";
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML =
+    '<img src="${response.data.condition.icon_url"class="weather-app-icon"/>';
 }
 
-function formatDate(date){
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let day = days[date.getDay()];
-if (minutes<10) {minutes ='0${minutes'};
-return '${day} ${hours} :${minutes}';
-
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = "0${minutes";
+  }
+  return "${day} ${hours} :${minutes}";
 }
 
+function searchCity(city) {
+  let apiKey = "f453aef40oa9243878d838b26aet3b6c";
+  let apiUrl =
+    " https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}";
 
-function searchCity(city){
-    let apiKey = f453aef40oa9243878d838b26aet3b6c
-    let apiUrl = https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey};
-    axios.get(apiUrl).then(updateWeather)
-
+  axios.get(apiUrl).then(updateWeather);
 }
 
-function handleSearchSubmit(event){
-event.preventDefault();
-let searchInput= document.querySelector("search-form-input");
-searchCity(searchInput.value);
-
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#weather-form-input");
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
-let searchFormElement=document.querySelector("#searchform");
-searchFormElement.addEventListener("submit,handleSearchSubmit");
-searchCity(Boksburg)
+
+let weatherFormElement = document.querySelector("#weatherForm");
+weatherFormElement.addEventListener("submit", handleSearchSubmit);
